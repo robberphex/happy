d19385b2d8882cce47b8dc36123cf927ff522ac6
Robert Lu <robberphex@gmail.com>
2026-03-05 12:50:22 +0800

    getRandomBytes

--- packages/happy-lark/routes/callback/happy-lark/lark/event/index.js
+++ packages/happy-lark/routes/callback/happy-lark/lark/event/index.js
@@ -1,10 +1,27 @@
+import { randomBytes } from 'crypto';
+
+function getRandomBytesAsync(size) {
+  return new Promise((resolve, reject) => {
+    randomBytes(size, (err, buffer) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(buffer);
+      }
+    });
+  });
+}
+
 export default async function (fastify, opts) {
   fastify.post('/', async function (request, reply) {
+    console.debug(JSON.stringify(request.body));
     const { type, challenge } = request.body
 
     if (type === 'url_verification') {
       return { challenge }
     }
+
+    const bytes = await getRandomBytesAsync(32);
 
     return { ok: true }
   })
